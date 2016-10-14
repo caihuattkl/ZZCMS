46ca08e3613b4c0ba04aa761c246a0c1494043fd
caihua <27247758@qq.com>
2016-10-14 16:38:39 +0800

    1.完善股票计算器公式

--- static/src/js/tool.js
+++ static/src/js/tool.js
@@ -1,1 +1,27 @@
-alert("111")
+$("#calcFn").click(function() {
+	var firstPrice = Number($("#firstPrice").val()),//首次买入价
+		firstNumber = Number($("#firstNumber").val()),//首次买入股数
+		BuyingPositions = Number($("#BuyingPositions").val()),//补仓买入价
+		BuyingStocks = Number($("#BuyingStocks").val()),//补仓买入股数
+		commissionRate = Number($("#commissionRate").val()),//手续费
+		totalCost;//补仓后成本价
+	if(0 !== firstPrice && 0 !== firstNumber && 0!== BuyingPositions && 0!== BuyingStocks && !isNaN(firstPrice) && !isNaN(firstNumber) && !isNaN(BuyingPositions) && !isNaN(BuyingStocks) && !isNaN(commissionRate)) {
+		//(首次买入价 * 首次买入股数 + 补仓买入价 * 补仓买入股数) / (首次买入股数 + 补仓买入股数);
+		totalCost=(firstPrice * firstNumber + BuyingPositions * BuyingStocks) /(firstNumber + BuyingStocks);
+		alert ("您补仓后成本价是:"+totalCost);
+		
+	} else {
+		alert("请将表单填写完整且必须填写数字")
+	}
+})
+
+
+function validIpt(){
+	
+}
+function resetFrom(){
+	$("#firstPrice").val("");
+	$("#firstNumber").val("");
+	$("#BuyingPositions").val("");
+	$("#BuyingStocks").val("");
+}
