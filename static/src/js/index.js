7baf72acb87eb11ad1a01ae0d5327c5e348aa674
caihua <27247758@qq.com>
2016-10-26 18:18:52 +0800

    1.编写首页轮播图片代码 2.将多个js合并压缩为1个js,提高性能

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -8,47 +8,61 @@ $('#section-left>ul>li').on({
 	}
 });
 
-//首页图片轮播
-$(function() {
-	var pics = [{
-		url: '../images/banner-1.jpg',
-		link: '#1',
-		time: 5000
-	}, {
-		url: '../images/banner-2.jpg',
-		link: '#2',
-		time: 4000
-	}, {
-		url: '../images/banner-3.jpg',
-		link: '#3',
-		time: 6000
-	}, {
-		url: '../images/banner-4.jpg',
-		link: '#4',
-		time: 6000
-	}];
-	
-	function solidBanner(){
-		$("#bannerBox ul li a").each(function(i,v){
-		var step=1;
-		if(step==1 && i==0){
-			$(this).children("img").attr('src',pics[i].url);
-		}else{
-			$(this).children("img").attr('src',pics[i].url);
-			$(this).attr("style","display: none;")
-		}
-	})
-	}
-	
-	setInterval(function(){
-		console.log("a");
-	},3000)
-	
-	
-})
+function autoPlay() {
+	var pictures = $("#pictures");
+	var items = $("#pictures li");
+	var newTitle= $("#bannerText .text")
+	var jump = $("#jump span");
+	var pre = $("#pre");
+	var next = $("#next");
+	var index = -1;
+	var ctrl = null;
 
+	// 显示一张图片
+	function showItem(turn) {
+		// 首先将所有图片透明度设为0
+		items.attr("style", "opacity:0;")
+
+		//判断点击上一页还是下一页
+		if(turn) {
+			index--;
+			index < 0 && (index = items.length - 1);
+		} else {
+			index++ && index > items.length - 1 && (index = 0);
+		}
+		
+		$(items[index]).attr("style", "opacity:1;");
+		//图片标题文字
+		newTitle.text($("#pictures li a img").eq(index).attr("alt"));
+
+	}
+
+	ctrl = setInterval(showItem, 2000);
+	//移入时清除滚动
+	pictures.add(pre).add(next).add(jump).mouseover(function() {
+		clearInterval(ctrl)
+	}).mouseout(function() {
+		ctrl = setInterval(showItem, 2000);
+	})
+
+	//图片上下翻页
+	pre.click(function() {
+		showItem(true); //上一张
+	})
+
+	next.click(function() {
+			showItem(false);
+	})
+	//根据数字编号进入某张图
+	jump.mouseover(function(e) {
+		showItem(index=$(this).index()+1);
+	})
+
+};
 
 $(function() {
 	//判断是否是ie8及以下版本
 	isIe(8) ? $('body').empty() && alert("本站不支持IE8及其以下浏览器访问,请更新至IE9或chome,火狐浏览器!") : '';
+	//首页图片轮播
+	autoPlay(); 
 })
